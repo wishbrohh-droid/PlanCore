2cdeaaa789c11b6d5a4517f6f715c6f4f5b31dcc
wishbrohh-droid <wishbrohh@gmail.com>
2026-03-08 20:56:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,32 @@
-const menu = document.getElementById("menu")
-const levelPage = document.getElementById("levelPage")
-const levelTitle = document.getElementById("levelTitle")
+const menu = document.getElementById("menu");
+const levelPage = document.getElementById("levelPage");
+const levelTitle = document.getElementById("levelTitle");
 
 function openLevel(level){
 
-menu.style.display="none"
-levelPage.classList.remove("hidden")
+menu.style.display="none";
+levelPage.classList.remove("hidden");
 
-levelTitle.innerText="Level "+level
-
-document.getElementById("story1").innerText=""
-document.getElementById("story2").innerText=""
-document.getElementById("story3").innerText=""
-document.getElementById("story4").innerText=""
-document.getElementById("story5").innerText=""
-document.getElementById("story6").innerText=""
-document.getElementById("story7").innerText=""
-document.getElementById("story8").innerText=""
-document.getElementById("story9").innerText=""
-document.getElementById("story10").innerText=""
+levelTitle.innerText="Level " + level;
 
 }
 
 function goBack(){
 
-menu.style.display="block"
-levelPage.classList.add("hidden")
+menu.style.display="block";
+levelPage.classList.add("hidden");
 
 }
 
 /* STAR BACKGROUND */
 
-const canvas=document.getElementById("universe")
-const ctx=canvas.getContext("2d")
+const canvas = document.getElementById("universe");
+const ctx = canvas.getContext("2d");
 
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-let stars=[]
+let stars = [];
 
 for(let i=0;i<250;i++){
 
@@ -52,26 +41,26 @@ speed:Math.random()*0.6
 
 function animate(){
 
-ctx.clearRect(0,0,canvas.width,canvas.height)
+ctx.clearRect(0,0,canvas.width,canvas.height);
 
-ctx.fillStyle="white"
+ctx.fillStyle="white";
 
 stars.forEach(star=>{
 
-star.y+=star.speed
+star.y+=star.speed;
 
 if(star.y>canvas.height){
-star.y=0
+star.y=0;
 }
 
-ctx.beginPath()
-ctx.arc(star.x,star.y,star.size,0,Math.PI*2)
-ctx.fill()
+ctx.beginPath();
+ctx.arc(star.x,star.y,star.size,0,Math.PI*2);
+ctx.fill();
 
 })
 
-requestAnimationFrame(animate)
+requestAnimationFrame(animate);
 
 }
 
-animate()
+animate();
